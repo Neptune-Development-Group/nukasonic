ebe69e81272c945579da0ad6df29fe4ec556771b
Creativious <timothyu120@gmail.com>
2024-02-24 15:29:05 -0500

    first commit

---
+++ src/main/java/com/neptunedevelopmentteam/nukasonic/Nukasonic.java
@@ -0,0 +1,10 @@
+package com.neptunedevelopmentteam.nukasonic;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Nukasonic implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/neptunedevelopmentteam/nukasonic/client/NukasonicClient.java
@@ -0,0 +1,11 @@
+package com.neptunedevelopmentteam.nukasonic.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NukasonicClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
