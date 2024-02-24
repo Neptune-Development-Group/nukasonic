4c0eb58f49e3d89c21bb4cdc9d32122e119fdcfc
Creativious <timothyu120@gmail.com>
2024-02-24 15:40:57 -0500

    added item registration

--- src/main/java/com/neptunedevelopmentteam/nukasonic/Nukasonic.java
+++ src/main/java/com/neptunedevelopmentteam/nukasonic/Nukasonic.java
@@ -1,10 +1,16 @@
 package com.neptunedevelopmentteam.nukasonic;
 
+import com.neptunedevelopmentteam.neptunelib.core.init_handlers.NeptuneInitHandler;
+import com.neptunedevelopmentteam.nukasonic.registration.NukaItems;
 import net.fabricmc.api.ModInitializer;
 
+import java.util.logging.Logger;
+
 public class Nukasonic implements ModInitializer {
+    public static final String MOD_ID = "nukasonic";
+    public static final Logger LOGGER = Logger.getLogger(MOD_ID);
     @Override
     public void onInitialize() {
-
+        NeptuneInitHandler.register(NukaItems.class, MOD_ID);
     }
 }
---
+++ src/main/java/com/neptunedevelopmentteam/nukasonic/registration/NukaItems.java
@@ -0,0 +1,6 @@
+package com.neptunedevelopmentteam.nukasonic.registration;
+
+import com.neptunedevelopmentteam.neptunelib.core.init_handlers.NeptuneItemInit;
+
+public class NukaItems implements NeptuneItemInit {
+}
