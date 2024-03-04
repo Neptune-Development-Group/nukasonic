d2b22b6bf4f397e16b3c03442d8d9a2ed23bde53
Monsterwaill <morganhickingbotham3@gmail.com>
2024-03-04 18:13:23 +0000

    added functionality to the uranium ore, as well as added quite a bit of stuff for future blocks

--- src/main/java/com/neptunedevelopmentteam/nukasonic/registration/NukaBlocks.java
+++ src/main/java/com/neptunedevelopmentteam/nukasonic/registration/NukaBlocks.java
@@ -5,11 +5,8 @@ import com.neptunedevelopmentteam.neptunelib.core.init_handlers.NeptuneBlockInit
 import com.neptunedevelopmentteam.neptunelib.core.itemsettings.NeptuneItemSettings;
 import com.neptunedevelopmentteam.nukasonic.Nukasonic;
 import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
 
 
 public class NukaBlocks implements NeptuneBlockInit {
-
-    public static final Block URANIUM_ORE = new Block(NeptuneBlockSettings.create().addItemSettings(new NeptuneItemSettings().group(() -> Nukasonic.NUKASONIC_ITEMS)));
-
+    public static final Block URANIUM_ORE = new Block(NeptuneBlockSettings.create().addItemSettings(new NeptuneItemSettings().group(() -> Nukasonic.NUKASONIC_ITEMS)).hardness(3));
 }
