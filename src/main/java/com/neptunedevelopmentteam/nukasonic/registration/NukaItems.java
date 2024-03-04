414de7e3111121ee9411f09c1f6898778023a304
Monsterwaill <morganhickingbotham3@gmail.com>
2024-03-04 00:17:40 +0000

    BABABOOEY

--- src/main/java/com/neptunedevelopmentteam/nukasonic/registration/NukaItems.java
+++ src/main/java/com/neptunedevelopmentteam/nukasonic/registration/NukaItems.java
@@ -5,12 +5,13 @@ import com.neptunedevelopmentteam.neptunelib.core.itemsettings.NeptuneItemSettin
 import com.neptunedevelopmentteam.nukasonic.Nukasonic;
 import com.neptunedevelopmentteam.nukasonic.items.PipboyItem;
 import com.neptunedevelopmentteam.nukasonic.items.StimpakItem;
+import com.neptunedevelopmentteam.nukasonic.items.UraniumChunkItem;
 import net.minecraft.item.Item;
 
 public class NukaItems implements NeptuneItemInit {
     public static final Item PIPBOY = new PipboyItem(new NeptuneItemSettings().maxDamage(1).group(() ->Nukasonic.NUKASONIC_ITEMS));
     public static final Item STIMPAK = new StimpakItem((new NeptuneItemSettings().maxDamage(1).group(() -> Nukasonic.NUKASONIC_ITEMS)));
     public static final Item SYRINGE = new Item(new NeptuneItemSettings().maxDamage(1).group(() -> Nukasonic.NUKASONIC_ITEMS));
-    public static final Item URANIUM_CHUNK = new Item(new NeptuneItemSettings().group(() -> Nukasonic.NUKASONIC_ITEMS));
+    public static final Item URANIUM_CHUNK = new UraniumChunkItem(new NeptuneItemSettings().group(() -> Nukasonic.NUKASONIC_ITEMS));
 
 }
