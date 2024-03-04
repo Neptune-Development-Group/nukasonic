e71dba11e0ef729f33487e2d23ec65902f6982b9
Monsterwaill <morganhickingbotham3@gmail.com>
2024-03-04 14:27:28 +0000

    added uranium ore and fixed the stimpak :D (model wise leaving the fun stuff like fixing the feature for noah)

--- src/main/java/com/neptunedevelopmentteam/nukasonic/registration/NukaBlocks.java
+++ src/main/java/com/neptunedevelopmentteam/nukasonic/registration/NukaBlocks.java
@@ -5,11 +5,11 @@ import com.neptunedevelopmentteam.neptunelib.core.init_handlers.NeptuneBlockInit
 import com.neptunedevelopmentteam.neptunelib.core.itemsettings.NeptuneItemSettings;
 import com.neptunedevelopmentteam.nukasonic.Nukasonic;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 
 
 public class NukaBlocks implements NeptuneBlockInit {
 
     public static final Block URANIUM_ORE = new Block(NeptuneBlockSettings.create().addItemSettings(new NeptuneItemSettings().group(() -> Nukasonic.NUKASONIC_ITEMS)));
-    public static final Block BEANS = new Block(NeptuneBlockSettings.create());
 
 }
