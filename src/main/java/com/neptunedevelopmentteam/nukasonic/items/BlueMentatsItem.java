b63940794a9309cf6658295885db479fae5975f6
TheArkNoah05 <55030669+Noach05@users.noreply.github.com>
2024-03-06 20:23:41 +0100

    men

--- src/main/java/com/neptunedevelopmentteam/nukasonic/items/BlueMentatsItem.java
+++ src/main/java/com/neptunedevelopmentteam/nukasonic/items/BlueMentatsItem.java
@@ -1,5 +1,6 @@
 package com.neptunedevelopmentteam.nukasonic.items;
 
+import net.fabricmc.fabric.api.item.v1.FabricItem;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
@@ -16,6 +17,7 @@ public class BlueMentatsItem extends Item {
     public TypedActionResult use(World world, PlayerEntity user, Hand hand) {
         ItemStack itemStack = user.getStackInHand(hand);
         //user.addStatusEffect(new StatusEffectInstance(StatusEffects.LUCK, 6000, 1));
+
         return TypedActionResult.success(itemStack);
     }
 }
