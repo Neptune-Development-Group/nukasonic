a488728a276b11c2c3f1e31239cac54220de632b
Creativious <timothyu120@gmail.com>
2024-05-22 05:03:34 -0400

    updated the mod to 1.20.6, all my datagen stuff is broken so I'll fix that after I sleep.

--- src/main/java/com/neptunedevelopmentteam/nukasonic/items/PipboyItem.java
+++ src/main/java/com/neptunedevelopmentteam/nukasonic/items/PipboyItem.java
@@ -1,10 +1,7 @@
 package com.neptunedevelopmentteam.nukasonic.items;
 
+import com.neptunedevelopmentteam.neptunelib.core.itemsettings.NeptuneItemSettings;
 import com.neptunedevelopmentteam.nukasonic.registration.NukaItems;
-import dev.emi.trinkets.api.SlotReference;
-import dev.emi.trinkets.api.TrinketItem;
-import dev.emi.trinkets.api.TrinketsApi;
-import dev.emi.trinkets.api.client.TrinketRenderer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
@@ -18,15 +15,9 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Arm;
 
-public class PipboyItem extends TrinketItem implements TrinketRenderer {
+public class PipboyItem extends Item{
     private BipedEntityModel<LivingEntity> model;
-    public PipboyItem(Settings settings) {
+    public PipboyItem(NeptuneItemSettings settings) {
         super(settings);
-    }
-
-    @Override
-    @Environment(EnvType.CLIENT)
-    public void render(ItemStack stack, SlotReference slotReference, EntityModel<? extends LivingEntity> contextModel, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, LivingEntity entity, float limbAngle, float limbDistance, float tickDelta, float animationProgress, float headYaw, float headPitch) {
-        ItemRenderer itemRenderer = MinecraftClient.getInstance().getItemRenderer();
     }
 }
